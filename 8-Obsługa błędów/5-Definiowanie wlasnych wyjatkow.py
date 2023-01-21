47c9d4a116cc314a61547d1b3b75f95a89a9a3a6
NorbertWatroba <norbert@watroba.net>
2023-01-21 15:00:06 +0100

    Definiowanie własnych wyjątków

--- 8-Obsługa błędów/5-Definiowanie wlasnych wyjatkow.py
+++ 8-Obsługa błędów/5-Definiowanie wlasnych wyjatkow.py
@@ -1,6 +1,23 @@
 import datetime as dt
 
 
+class TripException(Exception):
+    def __init__(self, text: str, description: str):
+        super().__init__(text)
+        self.description = description
+
+    def __str__(self):
+        return f'{super().__str__()}, description: {self.description}'
+
+
+class TripNameException(TripException):
+    pass
+
+
+class TripDateException(TripException):
+    pass
+
+
 class Trip:
     def __init__(self, symbol, title, start, end):
         self.symbol = symbol
@@ -9,8 +26,10 @@ class Trip:
         self.end = end
 
     def check_data(self):
-        assert self.title, 'Title is empty!'
-        assert self.start <= self.end, 'Start date is later than end date!'
+        if len(self.title) == 0:
+            raise TripNameException("Title is empty!", 'You need to name trip somehow')
+        if self.start > self.end:
+            raise TripDateException("Start date is later than end date!", 'start date has to be before end date')
 
     @classmethod
     def publish_offer(cls, trips):
@@ -20,13 +39,15 @@ class Trip:
         for trip in trips:
             try:
                 trip.check_data()
-            except ValueError as e:
+            except TripNameException as e:
                 list_of_errors.append("{}: {}".format(trip.symbol, str(e)))
-            except Exception as e:
+            except TripDateException as e:
                 list_of_errors.append("{}: {}".format(trip.symbol, str(e)))
 
-        assert not list_of_errors, f'The list of trips has errors: {list_of_errors}'
-        print('the offer will be published...')
+        if len(list_of_errors) > 0:
+            raise TripException('List of trips has errors', str(list_of_errors))
+        else:
+            print('the offer will be published...')
 
 
 trips = [
